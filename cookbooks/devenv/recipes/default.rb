c55a4f4cf5e450ef57ecb1a7e0a9a89627a01922
Joseph Pontin <JPontin@spartaglobal.com>
2020-01-21 10:59:54 +0000

    Can now vagrant up with VM almost complete

---
+++ cookbooks/devenv/recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook:: devenv
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+# updates apt sources and installs python and pip
+apt_update
+package ["python3.6", "python3-pip"]
+
+# Creates init folders to allow main.py to access subfolders
+file '/home/ubuntu/code/src/__init__.py' do
+  action :create
+end
+
+file '/home/ubuntu/code/src/csv_generators/__init__.py' do
+  action :create
+end
+
+file '/home/ubuntu/code/src/itjobswatch_html_readers/__init__.py' do
+  action :create
+end
+
+# Creates Downloads folder to output to
+directory '/home/vagrant/Downloads' do
+  action :create
+end
+
+# Installs requirements as listed in requirements.txt
+execute "install requirements" do
+  command "pip3 install -r /home/ubuntu/code/requirements.txt"
+end
+
+# Installs pytest to allow test to be run
+execute "install pytest" do
+  command "pip3 install pytest"
+end
